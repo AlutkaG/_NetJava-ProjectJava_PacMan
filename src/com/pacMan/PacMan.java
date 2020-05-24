7df3b4cc04a37ac5c5be89435d3058738b926293
AlutkaG <alagerlach@interia.pl>
2020-05-24 20:07:52 +0200

    Added start window

--- src/com/pacMan/PacMan.java
+++ src/com/pacMan/PacMan.java
@@ -1,4 +1,23 @@
 package com.pacMan;
 
-public class PacMan {
+import javax.swing.*;
+import java.awt.*;
+
+public class PacMan extends JFrame{
+    private JPanel startWindow;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+    public PacMan(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(startWindow);
+        this.pack();
+
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new PacMan();
+        frame.setVisible(true);
+    }
 }
